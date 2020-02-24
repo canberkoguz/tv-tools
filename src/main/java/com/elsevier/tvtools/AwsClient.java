0d49713737888f71d8f336a7be424e364a6487f1
Canberk Oguz <c.oguz@elsevier.com>
2020-02-24 17:25:14 +0100

    Add aws sqs integration

---
+++ src/main/java/com/elsevier/tvtools/AwsClient.java
@@ -0,0 +1,32 @@
+package com.elsevier.tvtools;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AwsClient {
+
+  @Value("${AWS_ACCESS_KEY}")
+  private String accessKey;
+  @Value("${AWS_SECRET_KEY}")
+  private String secretKey;
+
+  @Bean
+  public AmazonSQS awsSqsClient() {
+    AWSCredentials credentials = new BasicAWSCredentials(
+        accessKey,
+        secretKey);
+    return AmazonSQSClientBuilder.standard()
+        .withCredentials(new AWSStaticCredentialsProvider(credentials))
+        .withRegion(Regions.US_EAST_1)
+        .build();
+  }
+
+}
--- src/main/java/com/elsevier/tvtools/SlackController.java
+++ src/main/java/com/elsevier/tvtools/SlackController.java
@@ -1,5 +1,8 @@
 package com.elsevier.tvtools;
 
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,23 +10,36 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class SlackController {
+
+  private static final String SQS_URL = "https://sqs.us-east-1.amazonaws.com/804335263071/hackaton-tv-tool";
+  private static final String DISPLAY_COMMAND = "display";
+
+  private final AmazonSQS awsSqsClient;
 
   @RequestMapping(value = "/slack/display",
                   method = RequestMethod.POST,
                   consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
   public SlackResponse onReceiveDisplayCommand(@RequestParam("team_id") String teamId,
-                                             @RequestParam("team_domain") String teamDomain,
-                                             @RequestParam("channel_id") String channelId,
-                                             @RequestParam("channel_name") String channelName,
-                                             @RequestParam("user_id") String userId,
-                                             @RequestParam("user_name") String userName,
-                                             @RequestParam("command") String command,
-                                             @RequestParam("text") String text,
-                                             @RequestParam("response_url") String responseUrl) {
+                                               @RequestParam("team_domain") String teamDomain,
+                                               @RequestParam("channel_id") String channelId,
+                                               @RequestParam("channel_name") String channelName,
+                                               @RequestParam("user_id") String userId,
+                                               @RequestParam("user_name") String userName,
+                                               @RequestParam("command") String command,
+                                               @RequestParam("text") String text,
+                                               @RequestParam("response_url") String responseUrl) {
 
-    System.out.println(text);
-    return new SlackResponse("response:" + text);
+    if (DISPLAY_COMMAND.equals(command)) {
+      SendMessageRequest sendMessageStandardQueue = new SendMessageRequest()
+          .withQueueUrl(SQS_URL)
+          .withMessageBody(text)
+          .withDelaySeconds(0);
+      awsSqsClient.sendMessage(sendMessageStandardQueue);
+      return new SlackResponse("Sent to SQS!");
+    }
+    return new SlackResponse("invalid command: " + command);
   }
 
 }
